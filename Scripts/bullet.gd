dab453108fd3ca0cd266d5707f736e94dcdc7f58
magicpalm1234 <apalmu6@gmail.com>
2025-10-16 13:03:34 +0200

    Added an enemy
    you can shoot and kill enemies

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -4,9 +4,12 @@ class_name Bullet
 
 @export var enemy: Enemy
 
+@export var explotion: PackedScene
+
 @onready var tank_barrel: AnimatedSprite2D = $"../TankBarrel"
 @export var tank: Tank
 var speed := 100.0
+var damage := 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,5 +28,17 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if enemy:
-		enemy.position = Vector2(1,1)
+	if body.is_in_group("enemy") and body.has_method("take_damage"):
+		body.take_damage(damage)
+		destroy()
+
+func destroy():
+	$BulletTexture.hide()
+	$SmokeTrail.queue_free()
+	speed = 0
+	
+	var new_explotion = explotion.instantiate()
+	add_child(new_explotion)
+	await new_explotion.animation_finished
+	
+	queue_free()
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,11 +2,30 @@ extends Node2D
 
 class_name Enemy
 
+@export var explotions: PackedScene
+
+@export var health: float
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	add_to_group("enemy")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func take_damage(amount):
+	health -= amount
+	if health <= 0:
+		die()
+
+
+func die():
+	var new_explotions = explotions.instantiate()
+	add_child(new_explotions)
+	new_explotions.emitting = true
+	$AnimatedSprite2D.play("Idle")
+	await new_explotions.finished
+	queue_free()
