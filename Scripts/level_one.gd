0b046e691f54d5b7222fb014df805f9e0142d81b
magicpalm1234 <apalmu6@gmail.com>
2025-11-02 16:02:38 +0100

    something that works, somehow

--- Scripts/level_one.gd
+++ Scripts/level_one.gd
@@ -4,9 +4,15 @@ signal restart_pressed
 signal main_menu_pressed
 signal enemy_died
 
+var open_spawn_point1 = true
+var open_spawn_point2 = true
+var open_spawn_point3 = true
+
 var amount_of_enemies := 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	new_tank()
+	new_tank()
 	new_tank()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,6 +25,8 @@ func _process(delta: float) -> void:
 	$Label.text = str("Enemies: ",amount_of_enemies)
 	get_children()
 	
+
+
 
 func win():
 	var new_win_screen = preload("res://Scenes/win_screen.tscn").instantiate()
@@ -39,26 +47,61 @@ func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func _on_enemy_died() -> void:
+	
+	if is_in_group("enemy_spawn1"):
+		open_spawn_point1 = true
+	
+	if is_in_group("enemy_spawn2"):
+		open_spawn_point2 = true
+	
+	if is_in_group("enemy_spawn3"):
+		open_spawn_point3 = true
+	
 	amount_of_enemies -= 1
 
 
 func new_tank() -> void:
-	var randspawn = randi_range(1,4)
+	var randspawn = randi_range(1,3)
 	var new_enemy_tank = preload("res://Scenes/enemy.tscn").instantiate()
 	
 	add_child(new_enemy_tank)
 	new_enemy_tank.add_to_group("enemy")
 	
 	if randspawn == 1:
-		new_enemy_tank.position = $SpawnPositions/SpawnPosition1.position
+		if open_spawn_point1 == true:
+			open_spawn_point1 = false
+			new_enemy_tank.add_to_group("enemy_spawn1")
+			new_enemy_tank.position = $SpawnPositions/SpawnPosition1.position
+			new_enemy_tank.position -= Vector2(0,9)
+			amount_of_enemies += 1
+		else:
+			new_enemy_tank.queue_free()
 	
-	if randspawn == 2:
-		new_enemy_tank.position = $SpawnPositions/SpawnPosition2.position
+	elif randspawn == 2:
+		if open_spawn_point2 == true:
+			open_spawn_point2 = false
+			new_enemy_tank.add_to_group("enemy_spawn2")
+			new_enemy_tank.position = $SpawnPositions/SpawnPosition2.position
+			new_enemy_tank.position -= Vector2(0,9)
+			amount_of_enemies += 1
+		else:
+			new_enemy_tank.queue_free()
 	
-	if randspawn == 3:
-		new_enemy_tank.position = $SpawnPositions/SpawnPosition3.position
+	elif randspawn == 3:
+		if open_spawn_point3 == true:
+			open_spawn_point3 = false
+			new_enemy_tank.add_to_group("enemy_spawn3")
+			new_enemy_tank.position = $SpawnPositions/SpawnPosition3.position
+			new_enemy_tank.position -= Vector2(0,9)
+			amount_of_enemies += 1
+		else:
+			new_enemy_tank.queue_free()
 	
-	if randspawn == 4:
-		new_enemy_tank.position = $SpawnPositions/SpawnPosition4.position
-	
-	amount_of_enemies += 1
+
+
+
+
+
+
+
+pass
